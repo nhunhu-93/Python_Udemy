507c003d8a0e1927ef0910d077194e8038bf9f79
nhunhu-93 <tunhu388@gmail.com>
2024-01-02 18:16:24 +0700

    end day 25

--- Day25/US State/main.py
+++ Day25/US State/main.py
@@ -14,18 +14,26 @@ list_state = data_state["state"].to_list()
 guess_state = []
 
 while len(guess_state) < 50:
-    input_name = screen.textinput(title="Guess the State", prompt="What's another State name?")
+    input_name = screen.textinput(title=f"{len(guess_state)}/50 Guess the State", 
+                                  prompt="What's another State name?").title()
+    
+    if input_name == "Exit":
+        miss_state = []
+        for state in list_state:
+            if state not in guess_state:
+                miss_state.append(state)
+        new_data = pandas.DataFrame(miss_state)
+        new_data.to_csv("Day25/US State/miss_state_data")
+        break
     
     for name in list_state:
         if input_name == name:
             guess_state.append(input_name)
-            
             new_turtle = Turtle()
-            new_turtle.hideturtle()
             new_turtle.penup()
+            new_turtle.hideturtle()
             row_name = data_state[data_state["state"] == name]
             new_turtle.goto(int(row_name.x), int(row_name.y))
             new_turtle.write(name)
 
-screen.mainloop()
 
